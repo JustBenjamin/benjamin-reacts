22f4dfbe0cae7a22351202a7c84c17b105af6d3a
Benjamin Hove <benhove86@gmail.com>
2024-04-24 14:52:23 -0400

    Game page update

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import OpenAI from 'openai' 
 
 const Game = () => {
+
+//   const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+//   dangerouslyAllowBrowser: true
+//   })
+
+//  console.log(openai.apiKey)
+
   return (
+
+
     <div>
       <h1>Game</h1>
+      
     </div>
+    
   )
 }
 
